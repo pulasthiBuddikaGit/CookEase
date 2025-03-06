bf502f57a64cac70be7e36f0ab9f33e41c5f133c
Nisalka Jayasinghe <jayasinghenisalka@gmail.com>
2025-03-06 20:05:47 +0530

    Update _layout.jsx
    did some modifications to the bottom navbar

--- cookEase/app/(tabs)/_layout.jsx
+++ cookEase/app/(tabs)/_layout.jsx
@@ -5,37 +5,48 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs screenOptions={({
+      tabBarStyle: { backgroundColor: '#f0f0f0', height: 60 },
+      tabBarLabelStyle: { fontSize: 14 },
+      tabBarActiveTintColor: '#00796b',
+      tabBarInactiveTintColor: '#494949', 
+      })}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          title: 'CookEase',
+          tabBarIcon: ({ color }) => (<FontAwesome size={28} name="home" color={color} />)
+          ,
+          tabBarLabel: 'Home', 
         }}
       />
       <Tabs.Screen
         name="meal"
         options={{
-          title: 'Meal',
+          title: 'My Meals',
           tabBarIcon: ({ color }) =>( 
             <MaterialIcons name="ramen-dining" size={28} color={color} /> //when you use material icons you have replace all the capital letters by simple and replace spaces with dash
-          )  
+          ),
+          tabBarLabel: 'Meal',  
         }}
       />
         <Tabs.Screen
         name="diet"
         options={{
-          title: 'Diet',
+          title: 'My Diet Plans',
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="food" size={28} color={color} />
-          )        
+            <MaterialCommunityIcons name="food-apple" size={28} color={color} />
+          ),
+          tabBarLabel: 'Diet',         
         }}
       />
         <Tabs.Screen
         name="user"
         options={{
-          title: 'User',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user-o" color={color} />,
+          title: 'My profile',
+          tabBarIcon: ({ color }) => (<FontAwesome size={28} name="user-o" color={color} />),
+          tabBarLabel: 'Profile', 
+          
         }}
       />
     </Tabs>
