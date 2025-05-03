8e9ee78f9689a1e1b2d9c3536bc507755794ed83
bimashav <156497939+bimashav@users.noreply.github.com>
2025-05-03 11:13:05 +0530

    Update index.jsx

--- cookEase/app/(tabs)/index.jsx
+++ cookEase/app/(tabs)/index.jsx
@@ -142,14 +142,7 @@ export default function Index() {
               </TouchableOpacity>
             </View>
            
-            <TouchableOpacity style={styles.box2} activeOpacity={0.8}>
-              <Text style={styles.boxTitle2}>Total Calories Insight</Text>
-              <Text style={styles.boxSubtitle2}>Monitor monthley calorie count</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.box2} activeOpacity={0.8}>
-              <Text style={styles.boxTitle2}>Body Weight Insights</Text>
-              <Text style={styles.boxSubtitle2}>monitor your weight</Text>
-            </TouchableOpacity>
+            
             <View>
               <PieChartScreen />
               
